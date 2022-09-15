3793990cc74072d60be7200c51169a96a7625d3a
wangsl <wangsl2001@gmail.com>
2022-09-15 16:47:31 -0400

    add aws hpc 01

--- aws-post-01.bash
+++ aws-post-01.bash
@@ -1,6 +1,190 @@
-{
-  date
-  hostname 
-  echo "This is my test $(date)"
-} > /tmp/post.log
 
+function create_user_accounts_sw77_wang()
+{
+  local flag=
+  if [ "$1" == "yes" ]; then
+    flag=""
+  else
+    flag="-M"
+  fi
+
+  local line=
+  while read -r line; do
+    local IFS=":"
+    local line=($line)
+    local netID=${line[0]}
+    local uID=${line[2]}
+    local gID=${line[3]}
+    local name=${line[4]}
+    /usr/sbin/groupadd -g $gID $netID
+    /usr/sbin/useradd $flag -g $gID -s /bin/bash -u $uID -c "$name" $netID
+  done <<EOF
+sw77:x:1296493:1296493:Shenglong Wang:/home/sw77:/bin/bash
+wang:x:10015:10015:Shenglong Wang:/home/wang:/bin/bash
+EOF
+
+  unset IFS
+}
+
+function enable_gpu_profile()
+{
+  if [ ! -e /dev/nvidia0 ]; then return; fi
+  
+  if [[ "$GPU_PROFILE" != "YES" ]]; then return; fi
+
+  cat<<EOF > /usr/local/sbin/setup-gpu-profile.bash
+#!/bin/bash
+set -x
+#systemctl isolate multi-user
+modprobe -r nvidia_uvm nvidia_drm nvidia_modeset nvidia
+modprobe nvidia NVreg_RestrictProfilingToAdminUsers=0
+#systemctl isolate graphical
+EOF
+
+  chmod 755 /usr/local/sbin/setup-gpu-profile.bash
+
+  cat<<EOF > /etc/systemd/system/gpu-profile.service
+[Unit]
+Description=Enable GPU profile
+Requires=multi-user.target
+
+[Service]
+ExecStart=/usr/local/sbin/setup-gpu-profile.bash
+StandardOutput=file:/tmp/setup-gpu-profile.log
+StandardError=file:/tmp/setup-gpu-profile.err
+Type=oneshot
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    
+  systemctl enable gpu-profile.service
+  systemctl start gpu-profile.service
+}
+
+function monitor_gpu_utilizations()
+{
+  if [ ! -e /dev/nvidia0 ]; then return; fi
+    
+  cat<<EOF > /etc/systemd/system/gpu-utilizations.service
+[Unit]
+Description=monitor GPU utilizations
+Requires=multi-user.target 
+
+[Service]
+ExecStart=/usr/bin/nvidia-smi --query-gpu=index,timestamp,utilization.gpu --format=csv,nounits,noheader --loop=10
+StandardOutput=append:/tmp/gpu-utilizations.log
+StandardError=append:/tmp/gpu-utilizations.err
+#Type=oneshot
+#RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  systemctl enable gpu-utilizations.service
+  systemctl start gpu-utilizations.service
+
+  if [[ "$GPU_UTILIZATIONS_CHECKING" != "NO" ]]; then
+    cat <(crontab -l 2> /dev/null) <(echo "*/10 * * * * /share/apps/admins/scripts/check_gpu_utilizations.bash") |  egrep -v ^$ | crontab -
+  fi
+}
+
+function create_user_account()
+{
+  local line="<__GETENT_PASSWD__>"
+  
+  if [[ $line =~ ^\<__ ]]; then return; fi
+  
+  local flag="-M"
+  
+  local IFS=":"
+  local line=($line)
+  local netID=${line[0]}
+  local uID=${line[2]}
+  local gID=${line[3]}
+  local name=${line[4]}
+  /usr/sbin/groupadd -g ${gID} ${netID}
+  /usr/sbin/useradd ${flag} -g ${gID} -s /bin/bash -u ${uID} -c "${name}" ${netID}
+
+  unset IFS
+}
+
+function change_ssh_host_ecdsa_key()
+{
+  cat<<EOF > /etc/ssh/ssh_host_ecdsa_key
+-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIIT1MBGZpdymVEIAlqlCTUMXTMvS3Jp+tItcpJTBg3L9oAoGCCqGSM49
+AwEHoUQDQgAEZr9jYvV+QqBfQh2IXn8Pxu1Pv3uWXaMmzaEuFqG6J/+qyRou3AWu
+YsRDrIyREOvVegtn8XmNOBy9aaCsSoStwQ==
+-----END EC PRIVATE KEY-----
+EOF
+
+  cat<<EOF > /etc/ssh/ssh_host_ecdsa_key.pub
+ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGa/Y2L1fkKgX0IdiF5/D8btT797ll2jJs2hLhahuif/qskaLtwFrmLEQ6yMkRDr1XoLZ/F5jTgcvWmgrEqErcE=
+EOF
+
+  systemctl restart sshd.service
+}
+
+function gpu_initialize()
+{
+  if [ ! -e /dev/nvidia0 ]; then return; fi
+    
+  for((i=0; i<12; i++)); do
+    if [ -x /share/apps/local/bin/p2pBandwidthLatencyTest ]; then
+      /share/apps/local/bin/p2pBandwidthLatencyTest
+      break
+    else
+      sleep 5
+    fi
+  done
+}
+
+function setup_ram_disk()
+{
+  local ram_dir=/mnt/ram
+  mkdir -p ${ram_dir}
+  mount -t ramfs -o size=100M ramfs ${ram_dir}
+  chmod 1777 ${ram_dir}
+}
+
+function mount_readonly_persistent_disks()
+{
+  local disk=
+  for disk in /dev/disk/by-id/google-ro-*; do
+	  local name=$(basename $disk | sed -e 's/^google-ro-//')
+    local mnt_pt=/mnt/$name
+    /bin/mkdir -p $mnt_pt
+    /usr/bin/mount -o ro $disk $mnt_pt
+  done
+}
+
+function setup_nvgrid()
+{
+  if [[ "${SLURM_JOB_PARTITION}" == "nvgrid" ]]; then
+    systemctl restart gdm 
+  fi
+}
+
+function mount_scratch_tmp()
+{
+  echo "10.194.190.2:/scratch_tmp_fs /scratch_tmp nfs _netdev,defaults" >> /etc/fstab
+  mkdir -p /scratch_tmp
+  /bin/mount -a
+}
+
+#if [ -e /tmp/nyu-startup.log ]; then exit; fi
+
+touch /tmp/nyu-startup.log
+chmod 600 /tmp/nyu-startup.log
+
+{
+  set -x
+
+  mkdir -p /opt/singularity/mnt/{container,final,overlay,session}
+
+  create_user_accounts_sw77_wang
+
+} >> /tmp/nyu-startup.log 2>&1
