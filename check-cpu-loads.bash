cefa958980cc7743000c90dd72f023db027b8459
wangsl <wangsl2001@gmail.com>
2022-10-10 21:14:13 -0400

    add check-cpu-loads.bash

---
+++ check-cpu-loads.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+tmp=($(/usr/bin/uptime | sed -e 's/,/ /g' | awk '{print $(NF-2) " " $(NF-1) " " $NF }'))
+
+load5=${tmp[0]}
+load10=${tmp[1]}
+load15=${tmp[2]}
+
+criterion=0.8
+
+if [[ $(echo "${load5} < ${criterion} && ${load10} < ${criterion} && ${load15} < ${criterion}" | /usr/bin/bc) -eq 1 ]]; then
+  /usr/sbin/shutdown -h
+fi
